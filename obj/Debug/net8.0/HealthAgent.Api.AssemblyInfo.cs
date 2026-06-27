e1e9dc2d6ce75b01dc7987ba212b222cbbfd5a6c
soloStack-Dev <ownstack864@gmail.com>
2026-06-27 19:02:54 +0530

    file was updated

--- obj/Debug/net8.0/HealthAgent.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/HealthAgent.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthAgent.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ebdd581d9c0470b5ccb4d67826df79a06dac65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca4a8848784c43f09e181a125da567d74ce8307")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthAgent.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthAgent.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
